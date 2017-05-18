9a46f637788919ee0fbcbec739efb98411e8c5cc
dangthuong <ntdangthuong@gmail.com>
2017-05-18 18:49:17 +0700

    update cnd

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
-var myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap']);
+var app = angular.module('myApp', ['ngRoute', 'ui.bootstrap', 'textAngular']);
 
-myApp.config(function($routeProvider) {
+app.config(function($routeProvider) {
     $routeProvider
 
         .when('/', {
             templateUrl: 'views/home.html',
             controller: 'BooksController'
         })
-        .when('/all', {
-            templateUrl: 'views/books.html',
+        .when('/category', {
+            templateUrl: 'views/category.html',
             controller: 'BooksController'
         })
-
-    .when('/books/details/:id', {
-            templateUrl: 'views/book_details.html',
-            controller: 'BooksController'
-        })
-        .when('/books/category/:id', {
+        .when('/category/:id', {
             templateUrl: 'views/genre.html',
+            controller: 'BooksController'
+        })
+        .when('/book/details/:id', {
+            templateUrl: 'views/book_details.html',
             controller: 'BooksController'
         })
         .when('/search/:text', {
@@ -28,34 +27,35 @@ myApp.config(function($routeProvider) {
             templateUrl: 'views/cart.html',
             controller: 'BooksController'
         })
-        .when('/login', {
-            templateUrl: 'views/login.html',
-            controller: 'UserController'
-        })
-        .when('/admin', {
+
+    .when('/admin', {
             templateUrl: 'views/admin.html',
             controller: 'BooksController'
         })
-        .when('/edit/:id', {
-            templateUrl: 'views/edit.html',
+        .when('/add', {
+            templateUrl: 'views/addBook.html',
             controller: 'BooksController'
         })
-        .when('/user', {
+        .when('/edit/:id', {
+            templateUrl: 'views/editBook.html',
+            controller: 'BooksController'
+        })
+        .when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'BooksController'
+        })
+        .when('/signup', {
+            templateUrl: 'views/signup.html',
+            controller: 'BooksController'
+        })
+
+    .when('/user', {
             templateUrl: 'views/user.html',
             controller: 'BooksController'
         })
         .when('/contact', {
             templateUrl: 'views/contact.html',
-            controller: 'ContactController'
+            controller: 'BooksController'
         })
         .otherwise({ redirectTo: '/' });
 });
-
-
-// $.noConflict();
-// jQuery(document).ready(function() {
-//     jQuery(".uploadImg").on("click", function() {
-//         jQuery(".inputImg").click();
-
-//     });
-// })
--- controllers/books.js
+++ controllers/books.js
@@ -1,130 +1,118 @@
-var myApp = angular.module('myApp');
-
-myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
-    console.log('BooksController loaded...');
-    var root = 'https://green-web-bookstore.herokuapp.com';
+app.controller("BooksController", ['$scope', 'service', '$http', '$routeParams', '$location', '$anchorScroll', function($scope, service, $http, $routeParams, $location, $anchorScroll) {
 
     $scope.getBooks = function() {
-        $http.get(root + '/api/books/').success(function(response) {
+        $http.get(service.getBooks).success(function(response) {
             $scope.books = response;
-            $scope.viewby = 5;
-            $scope.totalItems = $scope.books.length;
-            $scope.currentPage = 1;
-            $scope.itemsPerPage = $scope.viewby;
-            $scope.maxSize = 10;
-            $scope.pageCount = function() {
-                return Math.ceil($scope.books.length / $scope.itemsPerPage);
-            };
-            $scope.$watch('currentPage + itemsPerPage', function() {
-                var begin = (($scope.currentPage - 1) * $scope.itemsPerPage),
-                    end = begin + $scope.itemsPerPage;
 
-                $scope.filteredBooks = $scope.books.slice(begin, end);
-            });
-            console.log($scope.filteredBooks)
+        })
+    };
+    $scope.getBook = function() {
+        var id = $routeParams.id;
+        console.log(id);
+        $http.get(service.getBooks + id).success(function(response) {
+            $scope.book = response;
+            console.log(response)
         });
+    };
+
+    $scope.addBook = function() {
+        console.log($scope.book);
+        var reqBook = {
+            method: 'POST',
+            url: service.getBooks,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: $scope.book
+        }
+        $http(reqBook).then(function() {
+            console.log('success')
+            window.location.href = '#/admin/';
+        })
+
     }
 
-    $scope.getBook = function() {
-            var id = $routeParams.id;
-            console.log(id);
-            $http.get(root + '/api/books/' + id).success(function(response) {
-                $scope.book = response;
-                console.log(response)
-            });
-        }
-        // add book
-    $scope.addBook = function() {
-            console.log($scope.book);
-            $http.post(root + '/api/books/', $scope.book).success(function(response) {
-                window.location.href = '#/all';
-            });
-        }
-        // end add book
-        // edit book
     $scope.updateBook = function() {
-            var id = $routeParams.id;
-            $http.put(root + '/api/books/' + id, $scope.book).success(function(response) {
-                window.location.href = '#/admin/';
-            });
-        }
-        // end edit book
-        // remove book
-    $scope.removeBook = function(id) {
-        $http.delete(root + '/api/books/' + id).success(function(response) {
+        var id = $routeParams.id;
+        $http.put(service.getBooks + id, $scope.book).success(function(response) {
             window.location.href = '#/admin/';
         });
     }
 
+    $scope.removeBook = function(id) {
+        $http.delete(service.getBooks + id).success(function(response) {
+            window.location.href = '#/admin/';
+        });
+    }
+
+    // date
+
+    $scope.open1 = function() {
+        $scope.popup1.opened = true;
+    };
+    $scope.popup1 = {
+        opened: false
+    };
+    $scope.open2 = function() {
+        $scope.popup2.opened = true;
+    };
+    $scope.popup2 = {
+        opened: false
+    };
+    // end date
+
 
     $scope.getGenres = function() {
-        $http.get(root + '/api/genres/').success(function(response) {
+        $http.get(service.getGenres).success(function(response) {
             $scope.genres = response;
-            var genreName = '';
-            for (var i = 0; i < genres.length; i++) {
-                if (genres[i]._id === id) {
-                    genreName = genres[i].name;
-                }
-            }
-        });
 
-    }
-    $scope.showGenres = false;
-    $scope.GenresFunc = function() {
-        $scope.showGenres = !$scope.showGenres;
-    }
+        })
+    };
+    $scope.getBanners = function() {
+        $http.get(service.getBanners).success(function(response) {
+            $scope.banners = response;
+        })
+    };
+
     $scope.getBookByGenre = function() {
         var id = $routeParams.id;
-        $http.get(root + '/api/books/genre/' + id).success(function(response) {
+        $http.get(service.getGenre + id).success(function(response) {
             $scope.books = response;
-        });
-    }
-    $scope.getBanners = function() {
-        $http.get(root + '/api/banners/').success(function(response) {
-            $scope.banners = response;
-        });
-    }
-    $scope.myInterval = 2500;
+            $scope.text = function() {
+                for (var i = 0; i < $scope.genres.length; i++) {
+                    if ($scope.genres[i]._id === $routeParams.genreId) {
 
-    // cmt
-    $scope.user = {
-        'userName': 'Thương Đăng',
-        'userAvatarUrl': 'http://sv1.upsieutoc.com/2017/05/11/FullSizeRender2.jpg',
-        'userEmail': 'ntdangthuong@gmail.com',
-        'address': '21/15 Vạn Kiếp, phường 3, quận Bình Thạnh',
-        'phone': '0931 334 424'
-    }
-    $scope.comment = {};
-    $scope.addComment = function(post) {
-            $scope.comment.date = Date.now();
-            $scope.comment.userName = $scope.user.userName;
-            $scope.comment.userAvatarUrl = $scope.user.userAvatarUrl;
-            post.comments.push($scope.comment);
-            var req = {
-                method: 'PUT',
-                url: "https://green-web-bookstore.herokuapp.com/api/books/" + $routeParams.itemId,
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                data: post
+                        return $scope.genres[i].name;
+
+                    }
+                }
+
             }
-            $http(req).then(function() {
-                    console.log('success')
-                },
+        })
+    }
+    $scope.myInterval = 2000;
 
-                function() {
-                    console.log('error')
-                });
-            // $http.put(bookservice.getBook + $routeParams.itemId, $scope.post).success(function(response) {
-            //     console.log('success')
-            // });
+    /*-----Search---*/
+    $scope.textSearch = $routeParams.text;
+    $scope.searchBy = 'search'
+    $scope.search = function() {
+        $http.get(service.getBooks + $scope.searchBy + '/' + $scope.textSearch).success(function(response) {
+            $scope.searchBook = response;
+            $scope.bigTotalItems = $scope.searchBook.length;
+        })
+    }
+    $scope.submitSearch = function() {
+        console.log(service.getBooks + $scope.searchBy + $scope.textSearch)
+        window.location.href = '#/search/' + $scope.textSearch;
+    }
+    $scope.showSearch = false;
+    $scope.SearchFunc = function() {
+        $scope.showSearch = !$scope.showSearch;
+    }
 
-            console.log(post);
+    /*-----Rating---*/
+    /*-----rate ---*/
 
-        }
-        // cmt
-
-    // rating
     $scope.max = 5;
     $scope.isReadonly = false;
     $scope.hoveringOver = function(value) {
@@ -133,31 +121,38 @@ myApp.controller('BooksController', ['$scope', '$http', '$location', '$routePara
     $scope.ratingStates = [
         { stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty' }
     ];
-    // end rating
-    /*---validate--*/
-    $scope.submitForm = function(isValid) {
 
-            // check to make sure the form is completely valid
-            if (isValid) {
-                alert('Vui lòng điền đầy đủ thông tin!');
-            }
+    /*-----End Rating---*/
+    /*-----User---*/
+    $scope.user = service.user;
+    /*-----cmt---*/
+    $scope.comment = {};
+    $scope.addComment = function(post) {
+        $scope.comment.date = Date.now();
+        $scope.comment.userName = $scope.user.userName;
+        $scope.comment.userAvatarUrl = $scope.user.userAvatarUrl;
+        post.comments.push($scope.comment);
+        var req = {
+            method: 'PUT',
+            url: service.books + $routeParams.itemId,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: post
         }
-        /*-----Search---*/
-    $scope.textSearch = $routeParams.text;
-    $scope.searchBy = 'search'
-    $scope.search = function() {
-        $http.get(root + '/api/books/' + $scope.searchBy + '/' + $scope.textSearch).success(function(response) {
-            $scope.searchBook = response;
-            $scope.bigTotalItems = $scope.searchBook.length;
-        })
-    }
-    $scope.submitSearch = function() {
-            console.log(root + '/api/books/' + $scope.searchBy + $scope.textSearch)
-            window.location.href = '#/search/' + $scope.textSearch;
-        }
-        // end search
+        $http(req).then(function() {
+                console.log('success')
+            },
 
-    // cart
+            function() {
+                console.log('error')
+            });
+        console.log(post);
+    };
+    /*--------Cart ---------*/
 
-    // end cart
-}]);
+    $scope.scroll = function() {
+        $anchorScroll();
+    };
+
+}])
